61fdfaf65653b5b21e4e82559b8dc67d26087a8c
Jennifer-I <jenniferewuzie27@gmail.com>
2024-05-13 00:37:38 +0100

    add create shipping address

--- src/main/java/com/jennifer/controller/ShippingAddressController.java
+++ src/main/java/com/jennifer/controller/ShippingAddressController.java
@@ -3,6 +3,7 @@ package com.jennifer.controller;
 import com.jennifer.dto.request.AddressRequest;
 import com.jennifer.dto.response.AddressResponse;
 import com.jennifer.entity.ShippingAddress;
+import com.jennifer.exception.CustomException;
 import com.jennifer.service.ShippingAddressService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -32,54 +35,6 @@ public class ShippingAddressController {
         }
     }
 
-    @PreAuthorize("hasRole('CUSTOMER')")
-    @DeleteMapping("/deleteShippingAddress/{id}")
-    public ResponseEntity<String> deleteShippingAddress(@PathVariable Long id) {
-        try {
-            shippingAddressService.deleteShippingAddress(id);
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            log.error("Error occurred while adding shipping address", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-//
-//    public ResponseEntity<String> editShippingAddress(@RequestBody AddressRequest addressRequest) {
-//        try {
-//            shippingAddressService.editShippingAddress(addressRequest);
-//            return ResponseEntity.ok().build();
-//        } catch (Exception e) {
-//            log.error("Error occurred while adding shipping address", e);
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-//        }
-//    }
 
-    @PreAuthorize("hasRole('CUSTOMER')")
-    @PatchMapping("/setDefaultShippingAddress/{id}")
-    public ResponseEntity<String> setDefaultShippingAddress(@PathVariable Long id) {
-        try {
-            shippingAddressService.setDefaultShippingAddress(id);
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            log.error("Error occurred while adding shipping address", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
 
-    @PreAuthorize("hasRole('CUSTOMER')")
-    @GetMapping("/getShippingAddress/{id}")
-    public ResponseEntity<AddressResponse> getShippingAddress(@PathVariable Long id) {
-        try {
-            return ResponseEntity.ok(shippingAddressService.getShippingAddress(id));
-        } catch (Exception e) {
-            log.error("Error occurred while adding shipping address", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-    @PreAuthorize("hasRole('CUSTOMER')")
-    @GetMapping("/getsAllShippingAddresses")
-    public ResponseEntity<List<AddressResponse>> getAllShippingAddresses() {
-        List<AddressResponse> shippingAddresses = shippingAddressService.getAllShippingAddresses();
-        return ResponseEntity.ok(shippingAddresses);
-    }
 }
--- src/main/java/com/jennifer/service/serviceImpl/ShippingAddressServiceImpl.java
+++ src/main/java/com/jennifer/service/serviceImpl/ShippingAddressServiceImpl.java
@@ -96,8 +96,6 @@ public class ShippingAddressServiceImpl implements ShippingAddressService {
 
 
 
-}
-
 
 
 
